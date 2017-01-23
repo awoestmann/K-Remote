caf1be2145df6ae09804a4f28500b54c60f03e8d
tischgrill <tischgrill@live.de>
2017-01-23 14:53:08 +0100

    Added Connection Page/Handler

--- K-Remote/K-Remote/MainPage.xaml.cs
+++ K-Remote/K-Remote/MainPage.xaml.cs
@@ -13,6 +13,10 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+//Debug
+using System.Data;
+using System.Diagnostics;
+
 // Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
 
 namespace K_Remote
@@ -24,7 +28,24 @@ namespace K_Remote
     {
         public MainPage()
         {
+            //Debug           
+            Debug.WriteLine("Entering Main");           
+           
             this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Button Clicked");
+            var buttonName = ((Button)sender).Name;
+            switch (buttonName)
+            {
+                case "button_connections":
+                    this.Frame.Navigate(typeof(Connections));
+                    break;
+
+                default: Debug.WriteLine("Button switch: default case"); break;
+            }                  
         }
     }
 }
---
+++ K-Remote/K-Remote/Utils/ConnectionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_Remote.Utils
+{
+    class ConnectionHandler
+    {
+        private static ConnectionHandler instance = null;
+
+        public static ConnectionHandler getInstance()
+        {
+            if(ConnectionHandler.instance != null)
+            {
+                return ConnectionHandler.instance;
+            }
+            else
+            {
+                ConnectionHandler.instance = new ConnectionHandler();
+                return ConnectionHandler.instance;
+            }
+        }
+    }
+}
